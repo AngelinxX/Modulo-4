8c564539f2006325ff4dc2c023f70889f267906d
AngelinxX <fajardo_aa@outlook.com>
2024-01-17 22:22:42 -0500

    Node js

---
+++ miPrimerProyecto/app.js
@@ -0,0 +1,44 @@
+const express=require("express");
+const app=express();
+const puerto=3001;
+const bodyParser=require("body-parser")
+//contacto id,nombre,apellido,celular
+
+app.use(bodyParser.json());
+
+app.use("/contactos",(request,response,next)=>{
+    console.log("headers:",request.headers);
+    console.log("body:",request.body);
+    next();
+});
+
+app.listen(puerto,()=>{
+    console.log("Servidor listo en el puerto "+3001);
+});
+
+app.get("/contactos",(request,response)=>{
+    const contactos=[
+        {id:1,nombre:"Angel",apellido:"Fajardo",celular:"0992276544"},
+        {id:2,nombre:"Jose",apellido:"Andrango",celular:"0992123544"},
+        {id:3,nombre:"Pepe",apellido:"Teran",celular:"0998723544"},
+    ];
+    console.log("ingresa al get");
+    response.send(contactos);
+})
+
+app.post("/contactos",(req,resp)=>{
+    req.body.id=99;
+    resp.send(req.body);
+});
+
+app.put("/contactos/:idParam",(req,resp)=>{
+    const id=req.params.idParam;
+    console.log("id",id);
+    resp.send(req.body);
+});
+
+app.delete("/contactos/:id",(req,resp)=>{
+    const id =req.params.id;
+    console.log("id:",id);
+    resp.send();
+});
