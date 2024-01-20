235789c11aae57773ef9052748c6d75e2f4fb1fc
AngelinxX <fajardo_aa@outlook.com>
2024-01-20 12:45:58 -0500

    Ejercicio contactos

---
+++ contactos/App.js
@@ -0,0 +1,18 @@
+import {NavigationContainer} from"@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import { ContactsList } from "./screens/ContactsList"; 
+
+export default function App() {
+  const Stackcontacts = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stackcontacts.Navigator>
+        <Stackcontacts.Screen
+          name="ContactListNav"
+          component={ContactsList}
+        />
+      </Stackcontacts.Navigator>
+    </NavigationContainer>
+  );
+}
+
